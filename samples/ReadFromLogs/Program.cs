897eb9e7c481aecff010d662b8972deea6fc4a64
Remco Brosky <remco.brosky@microsoft.com>
2024-10-26 15:06:07 +0200

    ReadFromLogs sample

---
+++ samples/ReadFromLogs/Program.cs
@@ -0,0 +1,50 @@
+﻿//  This sample is documented here
+// https://learn.microsoft.com/en-us/dotnet/api/overview/azure/monitor.query-readme?view=azure-dotnet
+
+
+using Azure;
+using Azure.Identity;
+using Azure.Monitor.Query;
+using Azure.Monitor.Query.Models;
+using Microsoft.Extensions.Configuration;
+
+var client = new LogsQueryClient(new DefaultAzureCredential());
+
+var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile("appsettings.ignored.json", optional: true, reloadOnChange: true);
+
+IConfigurationRoot configuration = builder.Build();
+
+var workspaceId = configuration["Workspace:Id"];
+
+Response<LogsQueryResult> response = await client.QueryWorkspaceAsync(
+    workspaceId,
+    "AppMetrics " +
+    "| where Name == 'Total Tokens'" +
+    "| project Properties['Client IP'], Name, Sum",
+    new QueryTimeRange(TimeSpan.FromDays(31)));
+
+//get the response table
+LogsTable table = response.Value.Table;
+
+//write fields
+foreach (var column in table.Columns)
+{
+    Console.Write(column.Name + ";");
+}
+
+Console.WriteLine();
+
+//write values
+var columnCount = table.Columns.Count;
+foreach (var row in table.Rows)
+{
+    for (int i = 0; i < columnCount; i++)
+    {
+        Console.Write(row[i] + ";");
+    }
+
+    Console.WriteLine();
+}
